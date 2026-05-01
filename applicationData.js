18caf365fd4c76acf9f18d8ecdb8f8b694367b43
Tshepo Moeletsi <39188870+thatkingmag@users.noreply.github.com>
2026-05-01 14:50:03 +0200

    Admin: Update Exactly how to take a good trade, on anything. 2 hours. in application list

--- applicationData.js
+++ applicationData.js
@@ -1,16 +1,137 @@
 const applicationData = [
-    { id: "App 1", name: "Application of Material 1", link: "https://youtu.be/BUilx_P30vE", category: "Application", status: "Not Started", notes: "", rating: 0 },
-    { id: "App 2", name: "Application of Material 2", link: "https://youtu.be/V4XQTBUTeQ4", category: "Application", status: "Not Started", notes: "", rating: 0 },
-    { id: "App 3", name: "Application of Material 3", link: "https://youtu.be/d0j1ws214Pg", category: "Application", status: "Not Started", notes: "", rating: 0 },
-    { id: "App 4", name: "Application of Material 4", link: "https://youtu.be/NqxFEWAszTY", category: "Application", status: "Not Started", notes: "", rating: 0 },
-    { id: "App 5", name: "Application of Material 5", link: "https://youtu.be/3OoV8yS6bCs", category: "Application", status: "Not Started", notes: "", rating: 0 },
-    { id: "App 6", name: "Application of Material 6", link: "https://youtu.be/PA8CPZj0Epc", category: "Application", status: "Not Started", notes: "", rating: 0 },
-    { id: "App 7", name: "Application of Material 7", link: "https://youtu.be/R8anv3OiqUE", category: "Application", status: "Not Started", notes: "", rating: 0 },
-    { id: "App 8", name: "Application of Material 8", link: "https://youtu.be/hkiQ1VTIZik", category: "Application", status: "Not Started", notes: "", rating: 0 },
-    { id: "App 9", name: "Application of Material 9", link: "https://youtu.be/NXMDHL5aISE", category: "Application", status: "Not Started", notes: "", rating: 0 },
-    { id: "App 10", name: "Application of Material 10", link: "https://youtu.be/Hgd48Tej-mk", category: "Application", status: "Not Started", notes: "", rating: 0 },
-    { id: "App 11", name: "Application Of Material 11", link: "https://youtu.be/vtmJQY3t7wI?si=5-bDCY6vRcP1y_VO", category: "Application", status: "Not Started", notes: "", rating: 0 },
-    { id: "App EVC HTF", name: "EVC HTF Application Of Material", link: "https://youtu.be/LedLYhF2zp0?si=ufi0IC2bI85Aqbat", category: "Application", status: "Not Started", notes: "", rating: 0 },
-    { id: "App 12", name: "APPLICATION OF MATERIAL 12", link: "https://youtu.be/76_LAlj62ds?si=ccKPelzCFjRM4xdW", category: "Application", status: "Not Started", notes: "", rating: 0 },
-    { id: "App LTF EVC", name: "Trading a LTF EVC with cycles", link: "https://youtu.be/V4XQTBUTeQ4", category: "Application", status: "Not Started", notes: "", rating: 0 }
+    {
+        "id": "App 1",
+        "name": "Application of Material 1",
+        "link": "https://youtu.be/BUilx_P30vE",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "App 2",
+        "name": "Application of Material 2",
+        "link": "https://youtu.be/V4XQTBUTeQ4",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "App 3",
+        "name": "Application of Material 3",
+        "link": "https://youtu.be/d0j1ws214Pg",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "App 4",
+        "name": "Application of Material 4",
+        "link": "https://youtu.be/NqxFEWAszTY",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "App 5",
+        "name": "Application of Material 5",
+        "link": "https://youtu.be/3OoV8yS6bCs",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "App 6",
+        "name": "Application of Material 6",
+        "link": "https://youtu.be/PA8CPZj0Epc",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "App 7",
+        "name": "Application of Material 7",
+        "link": "https://youtu.be/R8anv3OiqUE",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "App 8",
+        "name": "Application of Material 8",
+        "link": "https://youtu.be/hkiQ1VTIZik",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "App 9",
+        "name": "Application of Material 9",
+        "link": "https://youtu.be/NXMDHL5aISE",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "App 10",
+        "name": "Application of Material 10",
+        "link": "https://youtu.be/Hgd48Tej-mk",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "App 11",
+        "name": "Application Of Material 11",
+        "link": "https://youtu.be/vtmJQY3t7wI?si=5-bDCY6vRcP1y_VO",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "App EVC HTF",
+        "name": "EVC HTF Application Of Material",
+        "link": "https://youtu.be/LedLYhF2zp0?si=ufi0IC2bI85Aqbat",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "App 12",
+        "name": "APPLICATION OF MATERIAL 12",
+        "link": "https://youtu.be/76_LAlj62ds?si=ccKPelzCFjRM4xdW",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "App LTF EVC",
+        "name": "Trading a LTF EVC with cycles",
+        "link": "https://youtu.be/V4XQTBUTeQ4",
+        "category": "Application",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    },
+    {
+        "id": "exactly-how-to-take-a-good-trade,-on-anything.-2-hours.",
+        "name": "Exactly how to take a good trade, on anything. 2 hours.",
+        "link": "https://youtu.be/wwGYGyZYrtU",
+        "category": "System",
+        "status": "Not Started",
+        "notes": "",
+        "rating": 0
+    }
 ];
